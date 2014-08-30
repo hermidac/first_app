67e35bbbad61f9d120d3153bfd526ef33e37a34d
crpt <antiasics@gmail.com>
2014-08-30 10:16:17 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '908175dd02b8cb90a27685c4a708060aa0b3fcf3eb505ff79561db1134dabb69ef524ded5d2059efd80df5156dda1882e3eaf88b2e3e7a991224f435012ebc05'
